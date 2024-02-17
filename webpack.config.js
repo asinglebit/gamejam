0124c1937790f0db7e8796ffdfa93c3e3e533b74
Artem Trubachev <trubachev.artem@gmail.com>
2024-02-17 08:26:21 +0100

    Add marketing files (#3)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin")
 
 module.exports = (env) => ({
   entry: "./src/index.ts",
@@ -13,10 +13,7 @@ module.exports = (env) => ({
     rules: [
       {
         test: /\.ts$/,
-        use: [
-          { loader: "ts-loader" }, 
-          { loader: "ifdef-loader", options: { DEBUG: env.development } } 
-        ],
+        use: [{ loader: "ts-loader" }, { loader: "ifdef-loader", options: { DEBUG: env.development } }],
         exclude: /node_modules/,
       },
       { test: /\.(json|mp3)$/, use: "url-loader" },
@@ -28,17 +25,18 @@ module.exports = (env) => ({
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'src'),
+      directory: path.join(__dirname, "src"),
     },
+    https: true,
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ title:"Game Title", template: "./public/index.html" }),
+    new HtmlWebpackPlugin({ title: "Game Title", template: "./public/index.html" }),
     new CopyPlugin({
       patterns: [
         { from: "src/assets/audio", to: "assets/audio" },
         { from: "src/assets/textures", to: "assets/textures" },
       ],
-    })
+    }),
   ],
 })
